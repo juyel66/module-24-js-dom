c5ed10cdbd9931928316192e29471c87f4351bc3
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-02-24 13:18:41 +0600

    module 24

---
+++ js/dom.js
@@ -0,0 +1,13 @@
+const mains = document.querySelectorAll('main');
+// console.log(mains);
+for( const main of mains){
+    main.style.border = '2px solid red';
+main.style.marginBottom = '5px';
+main.style.borderRadius = '15px';
+main.style.backgroundColor = 'lightgray'
+}
+
+const main2= document.getElementById('main1')
+console.log(main2);
+
+
